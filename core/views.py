f2820d8f0d70d1ab9ee8eeb62c7a7846ee4b43ed
Nik <your@mail.com>
2025-09-02 18:51:53 +0200

    Update Task views to use TaskForm for form handling

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
+from core.forms import TaskForm
 from core.models import Tag, Task
 
 
@@ -12,12 +13,12 @@ class TaskListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = '__all__'
+    form_class = TaskForm
     success_url = reverse_lazy('core:task_list')
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = '__all__'
+    form_class = TaskForm
     success_url = reverse_lazy('core:task_list')
 
 class TaskDeleteView(generic.DeleteView):
